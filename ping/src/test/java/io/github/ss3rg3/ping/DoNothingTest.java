73b971e613ac00484269334d4f83a8ba93e215be
Klaus <no.mail.to.grab@here.com>
2022-07-31 12:17:54 +0200

    added dummy tests

---
+++ ping/src/test/java/io/github/ss3rg3/ping/DoNothingTest.java
@@ -0,0 +1,15 @@
+package io.github.ss3rg3.ping;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+@QuarkusTest
+public class DoNothingTest {
+
+    @Test
+    public void doNothing() {
+        // NO OP
+        // todo some tests would be nice...
+    }
+
+}
--- pong/src/test/java/io/github/ss3rg3/pong/DoNothingTest.java
+++ pong/src/test/java/io/github/ss3rg3/pong/DoNothingTest.java
@@ -7,15 +7,12 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
-public class GreetingResourceTest {
+public class DoNothingTest {
 
     @Test
-    public void testHelloEndpoint() {
-        given()
-          .when().get("/hello")
-          .then()
-             .statusCode(200)
-             .body(is("Hello from RESTEasy Reactive"));
+    public void doNothing() {
+        // NO OP
+        // todo some tests would be nice...
     }
 
 }
